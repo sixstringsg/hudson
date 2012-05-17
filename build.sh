c83c241b675391b58de8ccce8edcd814fade3d03
Jason Stewart <sixstringsg@verizon.net>
2012-05-17 14:37:42 -0500

    Get rid of the local manifest stuff.

--- build.sh
+++ build.sh
@@ -96,14 +96,6 @@ then
   . ~/.jenkins_profile
 fi
 
-cp $WORKSPACE/hudson/$REPO_BRANCH.xml .repo/local_manifest.xml
-
-echo Core Manifest:
-cat .repo/manifests/default.xml
-
-echo Local Manifest:
-cat .repo/local_manifest.xml
-
 echo Syncing...
 repo sync -d #> /dev/null 2> /tmp/jenkins-sync-errors.txt
 check_result "repo sync failed."
